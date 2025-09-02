e2a818ae2edca2111bede081979ec1f3d12d3876
Niilua <m.dzaki.mif@gmail.com>
2025-09-02 15:43:55 +0700

    update chart (progressive bar and final line chart)

--- Style.js
+++ Style.js
@@ -15,11 +15,15 @@ window.onload = function () {
             rotation: -90, // ensures top half
             circumference: 180, // draw 180° more (bottom half)
             plugins: {
-                legend: { display: false },
+                legend: {
+                    display: false
+                },
                 title: {
                     display: true,
                     text: 'Fair',
-                    font: { size: 20 }
+                    font: {
+                        size: 20
+                    }
                 }
             },
         }
@@ -32,8 +36,7 @@ window.onload = function () {
         type: "line",
         data: {
             labels: xValues,
-            datasets: [
-                {
+            datasets: [{
                     fill: false,
                     tension: 0.3,
                     borderColor: "rgba(17, 104, 255, 1)",
@@ -54,14 +57,26 @@ window.onload = function () {
             ]
         },
         options: {
-            plugins: { legend: { display: false } },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
             scales: {
-                x: { grid: { display: false } },
+                x: {
+                    grid: {
+                        display: false
+                    }
+                },
                 y: {
                     min: 0,
                     max: 100,
-                    grid: { display: true },
-                    ticks: {display: false}
+                    grid: {
+                        display: true
+                    },
+                    ticks: {
+                        display: false
+                    }
                 }
             }
         }
@@ -83,14 +98,26 @@ window.onload = function () {
             }]
         },
         options: {
-            plugins: { legend: { display: false } },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
             scales: {
-                x: { grid: { display: false } },
+                x: {
+                    grid: {
+                        display: false
+                    }
+                },
                 y: {
                     min: 6,
                     max: 10,
-                    grid: { display: true},
-                    ticks: {display: false}
+                    grid: {
+                        display: true
+                    },
+                    ticks: {
+                        display: false
+                    }
                 }
             }
         }
@@ -100,23 +127,59 @@ window.onload = function () {
         type: "line",
         data: {
             labels: ["2m", "4m", "8m", "12m", "24m", "48m"],
-            datasets: [
-                { fill: true, tension: 0, backgroundColor: "rgba(255, 97, 70, 0.7)", data: [2, 4, 5, 7, 8, 7] },
-                { fill: true, tension: 0, backgroundColor: "rgba(255, 187, 91, 0.7)", data: [5, 5, 7, 8, 10, 8] },
-                { fill: true, tension: 0, backgroundColor: "rgba(211, 255, 91, 0.7)", data: [7, 8, 10, 12, 11, 9] },
-                { fill: true, tension: 0, backgroundColor: "rgba(91, 255, 142, 0.7)", data: [10, 9, 12, 15, 15, 14] },
-                { fill: true, tension: 0, backgroundColor: "rgba(57, 222, 255, 0.7)", data: [15, 14, 18, 18, 18, 17] }
+            datasets: [{
+                    fill: true,
+                    tension: 0,
+                    backgroundColor: "rgba(255, 97, 70, 0.7)",
+                    data: [2, 4, 5, 7, 8, 7]
+                },
+                {
+                    fill: true,
+                    tension: 0,
+                    backgroundColor: "rgba(255, 187, 91, 0.7)",
+                    data: [5, 5, 7, 8, 10, 8]
+                },
+                {
+                    fill: true,
+                    tension: 0,
+                    backgroundColor: "rgba(211, 255, 91, 0.7)",
+                    data: [7, 8, 10, 12, 11, 9]
+                },
+                {
+                    fill: true,
+                    tension: 0,
+                    backgroundColor: "rgba(91, 255, 142, 0.7)",
+                    data: [10, 9, 12, 15, 15, 14]
+                },
+                {
+                    fill: true,
+                    tension: 0,
+                    backgroundColor: "rgba(57, 222, 255, 0.7)",
+                    data: [15, 14, 18, 18, 18, 17]
+                }
             ]
         },
         options: {
-            plugins: { legend: { display: false } },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
             scales: {
-                x: { grid: { display: false } },
+                x: {
+                    grid: {
+                        display: false
+                    }
+                },
                 y: {
                     min: 0,
                     max: 30,
-                    grid: { display: true},
-                    ticks: {display: false}
+                    grid: {
+                        display: true
+                    },
+                    ticks: {
+                        display: false
+                    }
                 }
             }
         }
@@ -128,28 +191,43 @@ window.onload = function () {
         type: "line",
         data: {
             labels: xValues3,
-            datasets: [
-                {
+            datasets: [{
+                    fill: true,
                     label: 'Volt Pass',
                     backgroundColor: "rgba(108, 162, 255, 0.7)",
+                    borderColor: "rgba(108, 162, 255, 1)",
                     data: [20, 25, 30, 22, 17, 20, 27, 30, 25, 20, 18, 15]
                 },
                 {
+                    fill: true,
                     label: 'DO',
-                    backgroundColor: "rgba(200, 206, 215, 0.7)",
-                    data: [15, 18, 20, 15, 10, 12, 15, 18, 20, 22, 25, 30]
+                    backgroundColor: "rgba(127, 244, 255, 0.7)",
+                    borderColor: "rgba(127, 244, 255, 1)",
+                    data: [25, 30, 35, 30, 27, 25, 30, 35, 30, 28, 25, 22]
                 }
             ]
         },
         options: {
-            plugins: { legend: { display: false } },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
             scales: {
-                x: { grid: { display: false } },
+                x: {
+                    grid: {
+                        display: false
+                    }
+                },
                 y: {
                     min: 0,
-                    max: 40,
-                    grid: { display: true },
-                    ticks: {display: false}
+                    max: 80,
+                    grid: {
+                        display: true
+                    },
+                    ticks: {
+                        display: false
+                    }
                 }
             }
         }
@@ -165,7 +243,15 @@ window.onload = function () {
                 backgroundColor: "rgba(108, 162, 255, 0.7)",
                 borderColor: "rgba(108, 162, 255, 1)",
                 borderWidth: 0.2,
-                borderRadius: 5,
+                borderRadius: 30,
+                borderSkipped: false,
+            },{
+                label: 'full',
+                data: [100],
+                backgroundColor: "rgba(200, 206, 215, 0.7)",
+                borderColor: "rgba(200, 206, 215, 1)",
+                borderWidth: 0.2,
+                borderRadius: 30,
                 borderSkipped: false,
             }]
         },
@@ -174,17 +260,33 @@ window.onload = function () {
                 title: {
                     display: true,
                     text: '75%',
-                    font: { size: 20 },
+                    font: {
+                        size: 20
+                    },
                     position: 'right',
                     align: 'center',
-                    
+
                 },
-                legend: { display: false },
-                tooltip: { enabled: false }
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    enabled: false
+                }
             },
             scales: {
-                x: { display: false, min: 0, max: 100 },
-                y: { display: false, min: 0, max: 100 }
+                x: {
+                    display: false,
+                    min: 0,
+                    max: 100
+                    
+                },
+                y: {
+                    display: false,
+                    min: 0,
+                    max: 100,
+                    stacked: true,
+                }
             },
             responsive: true,
             maintainAspectRatio: false,
@@ -195,4 +297,59 @@ window.onload = function () {
     // Update Chart6 data and refresh
     Chart6.data.datasets[0].data[0] = 85;
     Chart6.update();
+
+    new Chart(document.getElementById("Chart-7"), {
+        type: "line",
+        data: {
+            labels: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24],
+            datasets: [{
+                label: 'pH',
+                tension: 0.4,
+                data: [7, 7, 7, 7.6, 8, 9, 5, 6.5, 6, 7, 7.2, 7],
+                backgroundColor: "rgba(108, 162, 255, 0.7)",
+                borderColor: "rgba(108, 162, 255, 1)",
+                borderSkipped: false,
+            }, {
+                label: 'DO',
+                tension: 0.4,
+                data: [5, 6, 7, 6.5, 6, 5.5, 4, 3.5, 4, 5, 5.5, 6],
+                backgroundColor: "rgba(200, 206, 215, 0.7)",
+                borderColor: "rgba(200, 206, 215, 1)",
+                borderSkipped: false,
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: true
+                },
+                title: {
+                    display: true,
+                    font: {
+                        size: 20
+                    },
+                    position: 'right',
+                    align: 'center',
+
+                }
+            },
+            scales: {
+                x: {
+                    grid: {
+                        display: false
+                    },
+                },
+                y: {
+                    min: 0,
+                    max: 10,
+                    grid: {
+                        display: false
+                    },
+                    ticks: {
+                        display: false
+                    },
+                }
+            }
+        }
+    });
 };
